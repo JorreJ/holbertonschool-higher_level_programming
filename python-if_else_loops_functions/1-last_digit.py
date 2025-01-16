232a305f62c4006fef9abf435c3ff516f43dc926
JorreJ <jade.jorre@gmail.com>
2025-01-16 14:34:06 +0100

    add sign to last digit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,13 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    p_number = -number % 10
+    ld = -(-number % 10)
 else:
-    p_number = number % 10
-if p_number > 5:
-    print(f"Last digit of {number} is {p_number} and is greater than 5")
-elif p_number == 0:
-    print(f"Last digit of {number} is {p_number} and is 0")
+    ld = number % 10
+if ld > 5:
+    print(f"Last digit of {number} is {ld} and is greater than 5")
+elif ld == 0:
+    print(f"Last digit of {number} is {ld} and is 0")
 else:
-    print(f"Last digit of {number} is {p_number}\
+    print(f"Last digit of {number} is {ld}\
  and is less than 6 and not 0")
