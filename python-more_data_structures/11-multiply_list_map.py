87b84b24bf70a1ee442ebe5445761cc1d08a52a3
JorreJ <jade.jorre@gmail.com>
2025-01-26 10:24:11 +0100

    completely change multiply_list_map.py programm

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    new_list = my_list[:]
-    map(multiply(new_list, number), new_list)
-    return new_list
-
-
-def multiply(new_list, number):
-    for x in range(len(new_list)):
-        new_list[x] = new_list[x] * number
-    return new_list
+    return list(map(lambda x: x * number, my_list))
