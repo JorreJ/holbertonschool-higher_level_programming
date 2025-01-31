974298f0803c3ee1d0e3570b70349debb079adcd
JorreJ <jade.jorre@gmail.com>
2025-01-31 18:40:42 +0100

    create 0-rectangle.py programm

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module: rectangle
+Defines a class Rectangle that represents a rectangle.
+
+This module introduces a basic class definition in Python, serving as a
+template for creating rectangle objects. The class is currently empty and
+does not include any attributes, methods, or functionality. It serves as a
+starting point for further development.
+"""
+
+
+class Rectangle:
+    """
+    A class to represent a rectangle.
+
+    This is an empty class that can be expanded in future implementations
+    to include attributes such as width and height, along with methods to
+    calculate area, perimeter, and other rectangle properties.
+    """
+    pass
