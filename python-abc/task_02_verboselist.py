6018a05421797b1701c5ddc6484b76aa1303fa6e
JorreJ <jade.jorre@gmail.com>
2025-02-07 22:57:41 +0100

    add list function in extend to count items

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -3,16 +3,17 @@
 class VerboseList(list):
     def append(self, item):
         super().append(item)
-        print(f"Added {item} to the list.")
+        print(f"Added [{item}] to the list.")
 
     def extend(self, item):
-        super().extend(item)
-        print(f"Extended the list with {len(item)} items")
+        items = list(item)
+        super().extend(items)
+        print(f"Extended the list with [{len(items)}] items")
 
     def remove(self, item):
-        print(f"Removed {item} from the list")
+        print(f"Removed [{item}] from the list")
         super().remove(item)
 
     def pop(self, index=-1):
-        print(f"Popped {self[index]} from the list")
+        print(f"Popped [{self[index]}] from the list")
         super().pop(index)
