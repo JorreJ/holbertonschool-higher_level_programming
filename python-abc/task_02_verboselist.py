701b66ceff39311f62f6dd6eea51b40edb186335
JorreJ <jade.jorre@gmail.com>
2025-02-07 22:54:30 +0100

    remove square brackets

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -3,16 +3,16 @@
 class VerboseList(list):
     def append(self, item):
         super().append(item)
-        print(f"Added [{item}] to the list.")
+        print(f"Added {item} to the list.")
 
     def extend(self, item):
         super().extend(item)
-        print(f"Extended the list with [{len(item)}] items")
+        print(f"Extended the list with {len(item)} items")
 
     def remove(self, item):
-        print(f"Removed [{item}] from the list")
+        print(f"Removed {item} from the list")
         super().remove(item)
 
     def pop(self, index=-1):
-        print(f"Popped [{self[index]}] from the list")
+        print(f"Popped {self[index]} from the list")
         super().pop(index)
