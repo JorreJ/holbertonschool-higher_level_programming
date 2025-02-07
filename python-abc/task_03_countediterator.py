a39dc7187e6be922dea53ebe9c29b09d7d67f4d9
JorreJ <jade.jorre@gmail.com>
2025-02-07 23:21:01 +0100

    create task_03_countediterator.py programm

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+class CountedIterator:
+    def __init__(self, some_iterable):
+        self.iterator = iter(some_iterable)
+        self.count = 0
+    
+    def get_count(self):
+        return self.count
+    
+    def __next__(self):
+        self.count += 1
+        return next(self.iterator)
