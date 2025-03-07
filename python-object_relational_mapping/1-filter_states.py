9462426061e12c3b00dc6bb993b2c01e77361525
JorreJ <jade.jorre@gmail.com>
2025-03-07 23:38:43 +0100

    test another code

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,18 +1,38 @@
 #!/usr/bin/python3
-"""Module listing all states by N"""
+"""
+Lists all states with a name starting with N (upper N) from the database
+hbtn_0e_0_usa.
+Takes 3 arguments: mysql username, mysql password, and database name.
+Usage: ./1-filter_states.py <mysql_username> <mysql_password> <database_name>
+"""
+import sys
 import MySQLdb
-from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3]
-    )
-    cur = db.cursor()
-    query = """SELECT * FROM states WHERE name LIKE BINARY 'N%'
-                ORDER BY id ASC;"""
-    cur.execute(query)
-    rows = cur.fetchall()
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=mysql_username,
+                         passwd=mysql_password,
+                         db=database_name)
+
+    cursor = db.cursor()
+
+    query = """
+    SELECT *
+    FROM states
+    WHERE name LIKE BINARY 'N%'
+    ORDER BY id ASC;
+    """
+    cursor.execute(query)
+
+    rows = cursor.fetchall()
     for row in rows:
         print(row)
-    cur.close()
+
+    cursor.close()
     db.close()
