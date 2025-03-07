206ab4fe493511e5aacc875e0b64c306be253c98
JorreJ <jade.jorre@gmail.com>
2025-03-07 23:32:06 +0100

    make file executable

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,31 +1,16 @@
 #!/usr/bin/python3
-"""
-Lists all states from the database hbtn_0e_0_usa.
-"""
-
+"""Module listing all states from the database"""
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    # Connexion à la base de données
     db = MySQLdb.connect(
-        host="localhost",
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306
+        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3]
     )
-
-    # Création d'un curseur
     cur = db.cursor()
-
-    # Exécution de la requête SQL
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-
-    # Récupération et affichage des résultats
-    for row in cur.fetchall():
+    cur.execute("""SELECT * FROM states ORDER BY id""")
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
-
-    # Fermeture du curseur et de la connexion
     cur.close()
     db.close()
